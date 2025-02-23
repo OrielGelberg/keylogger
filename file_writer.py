9bee3e522ed34d6217b0715d41cf985c7e30c4b2
OrielGelberg <g9093587@gmail.com>
2025-02-23 13:43:01 +0200

    update whit new json

--- file_writer.py
+++ file_writer.py
@@ -1,7 +1,18 @@
-
-
+import json
+filename="keylog.json"
 class FileWriter:
 
-    def saveInJason(self):
-        pass
+        def save_to_json(self,data):
+
+            print(data)
+            try:
+                with open(filename, "w", encoding="utf-8") as file:
+                   json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+
+                print(f" הנתונים נשמרו בהצלחה ל-{filename}")
+            except Exception as e:
+                print(f" שגיאה בשמירת הקובץ: {e}")
+
 
